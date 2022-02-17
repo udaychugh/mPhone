246bb42a9cde55e980c04b99524569aa8eb52046
Uday Chugh <61309187+udaychugh@users.noreply.github.com>
2022-02-17 11:21:57 +0530

    Add files via upload

--- script/navbar.js
+++ script/navbar.js
@@ -1,29 +1,3 @@
-var newscript = document.createElement('script');  
-newscript.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js');
-document.head.appendChild(newscript);
-
-// ---------horizontal-navbar-menu-----------
-var tabsNewAnim = $('#navbar-animmenu');
-var selectorNewAnim = $('#navbar-animmenu').find('li').length;
-//var selectorNewAnim = $(".tabs").find(".selector");
-var activeItemNewAnim = tabsNewAnim.find('.active');
-var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
-var itemPosNewAnimLeft = activeItemNewAnim.position();
-$(".hori-selector").css({
-    "left": itemPosNewAnimLeft.left + "px",
-    "width": activeWidthNewAnimWidth + "px"
-});
-$("#navbar-animmenu").on("click", "li", function (e) {
-    $('#navbar-animmenu ul li').removeClass("active");
-    $(this).addClass('active');
-    var activeWidthNewAnimWidth = $(this).innerWidth();
-    var itemPosNewAnimLeft = $(this).position();
-    $(".hori-selector").css({
-        "left": itemPosNewAnimLeft.left + "px",
-        "width": activeWidthNewAnimWidth + "px"
-    });
-});
-
 document.getElementById("nav-scroll").style.top = "-100px"; 
 
 window.onscroll = function() {
